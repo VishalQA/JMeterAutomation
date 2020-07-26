9d8707f4db01bd2f314ac30e19a2de8595f14e46
VishalQA <vishal.mailbox1@gmail.com>
2020-07-26 20:00:26 +0530

    ok

---
+++ projectone/src/test/java/com/jmeterapi/projectone/TestClass3.java
@@ -0,0 +1,25 @@
+package com.jmeterapi.projectone;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class TestClass3 {
+	
+	@Test
+	public void hamcrestTest() {
+		given().contentType(ContentType.JSON)
+		.baseUri("https://reqres.in/api/users?page=2")
+		.when()
+		.get()
+		.then().log().all();
+	}
+	
+	
+
+}
